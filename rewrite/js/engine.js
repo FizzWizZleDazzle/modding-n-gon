b4afe52295215a9952b0d974215b299db2693289
Mentally Distressed Programmer <arturmisiurev@gmail.com>
2024-09-13 16:41:48 +0000

    setup 2

---
+++ rewrite/js/engine.js
@@ -0,0 +1,11 @@
+
+const Engine = Matter.Engine,
+    Events = Matter.Events,
+    Composites = Matter.Composites,
+    Composite = Matter.Composite,
+    Constraint = Matter.Constraint,
+    Vertices = Matter.Vertices,
+    Query = Matter.Query,
+    Body = Matter.Body,
+    Bodies = Matter.Bodies,
+    Vector = Matter.Vector;
